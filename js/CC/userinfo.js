e53c5bfda469078a4fc9ff3d66f5c192f807c782
dilsm56 <dilsm56@foxmail.com>
2017-10-16 21:17:14 +0800

    change

---
+++ js/CC/userinfo.js
@@ -0,0 +1,40 @@
+﻿var currentUser;
+
+$(function () {
+    initUserInfo();
+});
+
+function initUserInfo() {
+    try {
+        $.ajax({
+            type: 'post',
+            url: '/userinfo/GetCurrentUserInfo',
+            cache: false,
+            async: true,
+            data: {},
+            dataType: "json",
+            success: function (result) {
+                if (result.success) {
+                    $("#sayhello").html("Hi! " + result.name);
+                    $(".login-bt .btn").hide();
+                    $("#loginOut").show();
+                }
+                else {
+                    $(".login-bt .btn").show();
+                    $("#loginOut").hide();
+                }
+            },
+            error: function () {
+            }
+        });
+    }
+    catch (e) {
+        $("#sayhello").html("Hi! 你好");
+        $(".login-bt .btn").show();
+    }
+}
+function logout() {
+    $.removeCookie('Himall-User', { path: '/' });
+    $.removeCookie('Himall-SellerManager', { path: "/" });
+    location.reload();
+}
